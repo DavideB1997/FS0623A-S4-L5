96a4c94f384d4503e339959dfe4a920eafebf770
Davide Buglione <148747588+DavideB1997@users.noreply.github.com>
2023-11-24 16:55:02 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("scroll",function(){
+  let header = document.getElementById("header")
+  let bottone = document.getElementById("bottone")
+  let scroll = window.scrollY;
+
+  if(scroll> 290){
+    header.style.backgroundColor = "white";
+    bottone.style.backgroundColor = "green";
+  }else{
+    header.style.backgroundColor = "#ffc017";
+    bottone.style.backgroundColor = "black";
+  }
+})
+
+muroDiM();
+
+
+function muroDiM(){
+  let padre= document.getElementById("artwork")
+  let Ms = Array.from(padre.getElementsByTagName("g"))
+  let MsDone = []
+
+  setInterval(function(){
+    let randomNumber= Math.floor(Math.random()*Ms.length)
+
+    deselect(MsDone)
+
+    if(MsDone.includes(randomNumber)){
+      return
+    }else{
+      Ms[randomNumber].style.opacity = 0;
+      MsDone.push(randomNumber)
+    }
+
+  },1000)
+}
+
+function deselect(array){
+  array.forEach(element => {
+    element.style.opacity = 1;
+  });
+}
